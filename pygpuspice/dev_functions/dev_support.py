34818989652efabe8dbd3877c647d792297d691d
paeder92 <40853866+paeder92@users.noreply.github.com>
2020-08-08 12:01:41 +0200

    Update dev_support.py

--- pygpuspice/dev_functions/dev_support.py
+++ pygpuspice/dev_functions/dev_support.py
@@ -6,12 +6,12 @@ Created on Sat Aug  8 11:48:36 2020
 
 import networkx as nx
 
-def create_random_circuit(N):
+def create_random_circuit(N, R):
     
     """
     
-    This function shall create a random circuit of N resistors, which are interconnected randomly and
-    have random resistances. It returns the circuit as an instance of networkx.
+    This function shall create a random circuit of N nodes, which are interconnected randomly thorugh R resistors
+    of random resistances. It returns the circuit as an instance of networkx.
     
     """
     
